3a59174a249bfca49324775a137cb9e858d087db
Xiyang Chen <settinghead@gmail.com>
2015-11-17 00:29:53 -0500

    upgrade to latest garden snapshot

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC2"]
                  [garden "1.3.0-SNAPSHOT"]
                  [me.raynes/fs "1.4.4"]])
